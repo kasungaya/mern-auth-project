3c69172bee27ba320de30679bef99aed3b4dd57b
kasungaya <kasunice.s@gmail.com>
2023-11-29 14:56:38 +1030

    Created header and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import SignIn from "./Pages/SignIn.jsx";
+import SignUp from "./Pages/SignUp.jsx";
+import Profile from "./Pages/Profile.jsx";
+import Header from "./Components/Header.jsx";
 
 const App = () => {
     return (
-        <div className='text-red-500'>
-            App
-        </div>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/sign-in' element={<SignIn/>}/>
+                <Route path='/sign-up' element={<SignUp/>}/>
+                <Route path='/profile' element={<Profile/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
