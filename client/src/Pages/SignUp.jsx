e408deabc684cbff9896925223a74144580867b0
kasungaya <kasunice.s@gmail.com>
2023-12-09 21:34:41 +1030

    complete the sign in page functionality

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const SignUp = () => {
 
@@ -10,6 +10,7 @@ const SignUp = () => {
 
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -31,6 +32,7 @@ const SignUp = () => {
                 setError(true);
                 return;
             }
+            navigate('/sign-in');
         } catch (e) {
             setLoading(false);
             setError(true);
