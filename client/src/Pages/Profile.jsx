b9b0fc488f2814fa53033153a10f4d211d4ddeef
kasungaya <kasunice.s@gmail.com>
2023-12-13 07:27:08 +1030

    update upload functional and fixed bugs

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -48,7 +48,7 @@ const Profile = () => {
                 <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
                 <form className='flex flex-col gap-4'>
                     <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e) => setImage(e.target.files[0])}/>
-                    <img src={currentUser.profilePicture} alt="profile"
+                    <img src={formData.profilePicture || currentUser.profilePicture} alt="profile"
                          className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
                          onClick={() => fileRef.current.click()}/>
                     <p className='text-sm self-center'>
