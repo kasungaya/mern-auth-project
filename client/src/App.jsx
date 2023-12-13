556a1179d9b0ef8ff217f0daad901a10be892da3
kasungaya <kasunice.s@gmail.com>
2023-12-13 11:06:26 +1030

    bug fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
@@ -11,14 +11,14 @@ export default function App() {
     return (
         <BrowserRouter>
             {/* header */}
-            <Header/>
+            <Header />
             <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/sign-in' element={<SignIn/>}/>
-                <Route path='/sign-up' element={<SignUp/>}/>
-                <Route element={<PrivateRoute/>}>
-                    <Route path='/profile' element={<Profile/>}/>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/sign-in' element={<SignIn />} />
+                <Route path='/sign-up' element={<SignUp />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path='/profile' element={<Profile />} />
                 </Route>
             </Routes>
         </BrowserRouter>
